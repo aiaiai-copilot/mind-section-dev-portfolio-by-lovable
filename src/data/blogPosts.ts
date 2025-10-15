72e5e435045792bc8e8cafc40cc0370c632c1f83
softspider <sftspider@gmail.com>
2025-10-15 20:59:11 +0300

    feat: update  blog post

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -76,7 +76,7 @@ I should mention that by the time Vibe Coding gained popularity, I already had a
 
 Back in 2023, on a company project, we needed a small logger for a client application. I didn't want to bother with connecting heavy-duty tools like Sentry, so I quickly developed my own, using the recently released, but already sensational, ChatGPT 3.5. Naturally, this involved copying and pasting the code from the chat into the IDE, as there were no other options back then.
 
-The task itself was minimal, but I wanted to shift the main burden of implementation review onto tests. The model and I completed this in a TDD mode—the classic red-green-refactor cycle. Consequently, my manual review of the LLM-generated code was very brief, while test runs were quite numerous. In the end, everything went well; the logger was implemented and successfully deployed.
+The task itself was minimal, but I wanted to shift the main burden of implementation review onto tests. The model and I completed this in a TDD mode — the classic red-green-refactor cycle. Consequently, my manual review of the LLM-generated code was very brief, while test runs were quite numerous. In the end, everything went well; the logger was implemented and successfully deployed.
 
 It's worth noting here that good tests are contract tests — those created to verify not the entire codebase, but only a specific part of the contract implemented by the program. This contract might be documented, but it can also be captured in the contract tests themselves, which then serve as a living specification.
 
